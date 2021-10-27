5a5409d3d9fd7365900b9a968570ffd3cb4ab296
Andrey Zhilin <komplayer31@yandex.ru>
2021-10-27 12:45:12 +0300

    Created UI

--- Controls practice/ViewController.swift
+++ Controls practice/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    // OutcLet Collection коллекция switch (массив)
+    // перетягиваем из кода на все switch
+    // создаем таги ввиде степени двойки
+    @IBOutlet var switches: [UISwitch]!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    // actions
+    @IBAction func buttonPressed() {
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+    }
+    
+    @IBAction func sliderMoved() {
+    }
+    
+    @IBAction func textFieldEdited() {
+    }
+    
 }
 
