1876b4f24a880d39a0f0a28cdb400c1214d18956
Andrey Zhilin <komplayer31@yandex.ru>
2021-10-27 13:09:50 +0300

    Implemented number for button, slider and text field

--- Controls practice/ViewController.swift
+++ Controls practice/ViewController.swift
@@ -17,23 +17,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
     
+    // 8-битное число без знака
+    var number: UInt8 = 128 {
+        didSet{
+            updateUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    
+    /// Updates all outlets to number
+    func updateUI() {
+        button.setTitle("\(number)", for: [])
+        
+        // TODO: set switches to number
+        
+        slider.value = Float(number)
+        textField.text = "\(number)"
     }
 
     
     // actions
     @IBAction func buttonPressed() {
+        print(#line, #function)
+        number = UInt8((Int(number) + 1) % 256)
     }
     
     @IBAction func switchToggled(_ sender: UISwitch) {
+        print(#line, #function, sender.tag)
     }
     
     @IBAction func sliderMoved() {
+        print(#line, #function)
+        number = UInt8(slider.value)
     }
     
     @IBAction func textFieldEdited() {
+        print(#line, #function)
+        if textField.text == "" {
+            number = 0
+            textField.text = "0"
+        }
+        number = UInt8(textField.text ?? "") ?? 128
     }
     
 }
